9f354a7db611ea14847961d7f3d2270315f98419
nvs123456 <nvs882004@gmail.com>
2023-09-20 08:58:08 +0700

    just test main view

--- src/main/java/com/example/ourdictionary/Main.java
+++ src/main/java/com/example/ourdictionary/Main.java
@@ -11,13 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(),400,300);
         stage.setTitle("Dictionary");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
