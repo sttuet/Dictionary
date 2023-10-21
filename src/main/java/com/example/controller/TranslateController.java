6b8d9c4a7d2a45b67cf489c6adde91ed70589dec
Thinhtrang <nguyenvthinh244@gmail.com>
2023-10-22 01:21:19 +0700

    update button hover effect, slide transition, scroll bar

--- src/main/java/com/example/controller/TranslateController.java
+++ src/main/java/com/example/controller/TranslateController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.ourdictionary.Main;
 import com.example.service.ParseJSON;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,51 +11,69 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 
-public class TranslateController   {
+public class TranslateController {
+    @FXML
+    Label sourceLanguageLabel;
+    @FXML
+    Label targetLanguageLabel;
+    @FXML
+    private AnchorPane anchorPane;
     private Stage stage;
     private Scene scene;
     @FXML
     private TextField inputText;
     @FXML
     private TextArea translateResult;
-    private boolean engToViet=true;
+    private boolean engToViet = true;
+
     @FXML
     protected void onCloseButtonClick(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader=new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene=new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(Main.class.getResource("MainView.css").toExternalForm());
-        stage.setScene(scene);
-        stage.show();
+        TranslateTransition translateTransition = new TranslateTransition(Duration.millis(1000), anchorPane);
+        translateTransition.setFromX(0);
+        translateTransition.setToX(600);
+        translateTransition.setOnFinished((ActionEvent event1) -> {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            try {
+                scene = new Scene(fxmlLoader.load());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            scene.getStylesheets().add(Main.class.getResource("MainView.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+        });
+        translateTransition.play();
+
     }
+
     @FXML
     protected void onTranslate() throws IOException {
-        String s=inputText.getText();
-        if(engToViet){
-            String res= ParseJSON.transToViet(s);
+        String s = inputText.getText();
+        if (engToViet) {
+            String res = ParseJSON.transToViet(s);
             translateResult.setText(res);
-        }else {
-            String res= ParseJSON.transToEng(s);
+        } else {
+            String res = ParseJSON.transToEng(s);
             translateResult.setText(res);
         }
 
     }
+
     @FXML
-    Label sourceLanguageLabel;
-    @FXML
-    Label targetLanguageLabel;
-    @FXML
-    protected void exchangeLanguage(){
-        engToViet=!engToViet;
-        if(engToViet){
+    protected void exchangeLanguage() {
+        engToViet = !engToViet;
+        if (engToViet) {
             sourceLanguageLabel.setText("ANH");
             targetLanguageLabel.setText("VIET");
-        }else {
+        } else {
             sourceLanguageLabel.setText("VIET");
             targetLanguageLabel.setText("ANH");
         }
